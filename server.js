e984ed68817bda764bb9b9ed20fc239e557d87c9
Rian Ruggles <rs.ruggles@gmail.com>
2017-11-04 00:51:49 -0500

    Update language

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var middleWare = {
 app.use(middleWare.logger);
 
 app.get('/about', middleWare.requireAuthentication, function (req, res) {
-  res.send('About Us');
+  res.send('About Us!');
 });
 
 app.use(express.static(__dirname + '/public'));
